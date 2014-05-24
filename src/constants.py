e6437f9faff3b2ed0af2ce22d2ae4ae2d4803c1c
manishc1 <manishc1@umbc.edu>
2014-05-24 00:31:50 -0400

    CVE Id changed to current time.

--- src/constants.py
+++ src/constants.py
@@ -2,6 +2,13 @@
 Contains all the constants.
 """
 
-NVD_RSS_LIST = 'rss-list'
+import os
 
 XML_HEAD = '<?xml version="1.0" encoding="UTF-8"?>\n'
+
+CONFIG_DIR = os.path.abspath(__file__ + '/../../config')
+DATA_DIR = os.path.abspath(__file__ + '/../../data')
+
+NVD_RSS_LIST_FILE = CONFIG_DIR + '/nvd_rss_list.cfg'
+
+print NVD_RSS_LIST_FILE
--- src/nvd.py
+++ src/nvd.py
@@ -5,54 +5,55 @@ Details to grab the NVD vulnerabilities.
 """
 
 from constants import *
+from datetime import datetime
+from lxml import etree
+
 import feedparser
 import lxml.builder as lb
-from lxml import etree
-import sys, os
-
-DIR = os.path.abspath(os.curdir)#os.path.dirname(__file__)
-#sys.path.append(os.path.realpath('..'))
-
-
-def getRSSList():
-	f = open(NVD_RSS_LIST, 'r')
-	rssList = [line for line in f]
-	f.close()
-	return rssList
+import sys
 
 
 def parse(url):
+	"""
+	Extract all the vulnerabilities to seperate files.
+	"""
 	feed = feedparser.parse(url)
-	vulId = 0	
 	for post in feed.entries:
-		vulId = vulId+1
-		vulType = "CVE_Vulnerability"
+		vulId = 'CVE_' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
+		vulType = 'CVE_Vulnerability'
 		vulDate = post[u'vuln_published-datetime']
 		vulTitle = post[u'vuln_cve-id']
 		vulDesc = post[u'vuln_summary']
 
-		nstext = "new story"
 		document = lb.E.Document(
 			lb.E.Title(vulTitle),
 			lb.E.Date(vulDate),
 			lb.E.Description(vulDesc),
-			id="CVE_"+str(vulId), type=vulType)		
+			id=vulId, type=vulType)		
 		doc = etree.tostring(document, pretty_print=True)
 
-		# TODO: Resolve relative file path
-		filename = DIR + '/Data/CVE/CVE_'+str(vulId)+'.xml'
-		f = open(filename, 'w')
-		f.write(XML_HEAD)
-		f.write(doc)
-		f.close()
+		filename = DATA_DIR + '/CVE/CVE_' + vulId + '.xml'
+		writeString(filename, XML_HEAD + '\n' + doc)
+
 
 def parseList(rssList):
+	"""
+	Parses the rss list.
+	"""
 	for url in rssList:
 		parse(url)
 
+
 def main():
-	rssList = getRSSList()
+	"""
+	Main function.
+	"""
+	rssList = readLines(NVD_RSS_LIST_FILE)
 	parseList(rssList)
 
+
 if __name__ == "__main__":
+	"""
+	Entry point.
+	"""
 	main()
